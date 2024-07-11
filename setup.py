6ed246e3f7d547841983b09e590bd17374261d60
Link47750 <link4716@hotmail.com>
2024-07-11 08:12:13 +0200

    UPDATE

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# ONLINE MYSQL
+"""# ONLINE MYSQL
 app.config["MYSQL_HOST"] = "sql7.freesqldatabase.com"
 app.config["MYSQL_USER"] = "sql7718001"
 app.config["MYSQL_PASSWORD"] = "A5z86wrGha"
-app.config["MYSQL_DB"] = "sql7718001"
+app.config["MYSQL_DB"] = "sql7718001"""
 
-"""app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "data"""
+app.config["MYSQL_DB"] = "data"
 
 app.secret_key = 'secret_key'
 
